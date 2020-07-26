b2f1b0a24521e299c6c7e2ed4aad3600f5a7cf0f
Bibek Koirala <bibek.high@gmail.com>
2020-07-27 00:37:25 +0530

    'Topological_Sort_Problem'

--- Dungeon_problem_BFS.py
+++ Dungeon_problem_BFS.py
@@ -1,16 +1,33 @@
 import numpy as np
 
+# Initial state of the problem '.' represents free path, '#' represents blocked path, 'E' represents exit
 state = np.array([['.', '.', '.', '#', '.', '.', '.','.'],
                   ['.', '#', '.', '.', '.', '#', '.','.'],
                   ['.', '#', '.', '.', '.', '.', '.','#'],
-                  ['.', '.', '#', '#', '.', '.', 'E','.'],
-                  ['#', '.', '#', '.', '.', '#', '.','#']])
+                  ['.', '.', '#', '#', '.', '.', '.','.'],
+                  ['#', '.', '#', 'E', '.', '#', '.','#']])
 
+
+# Array to keep track of visited nodes
 visited = np.array([[False, False, False, False, False, False, False, False],
                     [False, False, False, False, False, False, False, False],
                     [False, False, False, False, False, False, False, False],
                     [False, False, False, False, False, False, False, False],
                     [False, False, False, False, False, False, False, False]])
+
+
+path_array = np.array([[False, False, False, False, False, False, False, False],
+                    [False, False, False, False, False, False, False, False],
+                    [False, False, False, False, False, False, False, False],
+                    [False, False, False, False, False, False, False, False],
+                    [False, False, False, False, False, False, False, False]])
+
+# Array to keep track of previous element
+prev = np.array([[None for item in range(0,8)],
+                [None for item in range(0,8)],
+                [None for item in range(0,8)],
+                [None for item in range(0,8)],
+                [None for item in range(0,8)]])
 
 ini_row = 0
 ini_col = 0
@@ -18,13 +35,17 @@ ini_col = 0
 state_row = 4
 state_col = 7
 
+path = (4, 3)
 
+
+# Node for tipple that defines tuple
 class Node:
     def __init__(self, pos):
         self.pos = pos
         self.next = None
 
 
+# Queue and its operations for BFS traversal
 class Queue:
     def __init__(self):
         self.head = None
@@ -52,6 +73,7 @@ class Queue:
             return top
 
 
+# Main BFS logic
 def BFS():
     main_queue.enqueue(Node((ini_row, ini_col)))
     i = 0
@@ -69,32 +91,46 @@ def BFS():
         i += 1
 
 
+# Functon to trace to new positions
 def findPath(ind):
     if ind[0] != 0:
         new_pos = (ind[0] - 1, ind[1])
         if not (visited[new_pos[0]][new_pos[1]] or state[new_pos[0]][new_pos[1]] == '#'):
             visited[new_pos[0]][new_pos[1]] = True
             main_queue.enqueue(Node(new_pos))
+            parent_pos = ind
+            child_pos = new_pos
+            prev[child_pos[0], child_pos[1]] = parent_pos
 
     if ind[1] != 0:
         new_pos = (ind[0], ind[1] - 1)
         if not (visited[new_pos[0]][new_pos[1]] or state[new_pos[0]][new_pos[1]] == '#'):
             visited[new_pos[0]][new_pos[1]] = True
             main_queue.enqueue(Node(new_pos))
+            parent_pos = ind
+            child_pos = new_pos
+            prev[child_pos[0], child_pos[1]] = parent_pos
 
     if ind[1] != state_col:
         new_pos = (ind[0], ind[1] + 1)
         if not (visited[new_pos[0]][new_pos[1]] or state[new_pos[0]][new_pos[1]] == '#'):
             visited[new_pos[0]][new_pos[1]] = True
             main_queue.enqueue(Node(new_pos))
+            parent_pos = ind
+            child_pos = new_pos
+            prev[child_pos[0], child_pos[1]] = parent_pos
 
     if ind[0] != state_row:
         new_pos = (ind[0] + 1, ind[1])
         if not (visited[new_pos[0]][new_pos[1]] or state[new_pos[0]][new_pos[1]] == '#'):
             visited[new_pos[0]][new_pos[1]] = True
             main_queue.enqueue(Node(new_pos))
+            parent_pos = ind
+            child_pos = new_pos
+            prev[child_pos[0], child_pos[1]] = parent_pos
 
 
+# Function to check if given node is the solution
 def checksol(val):
     if state[val[0]][val[1]] == 'E':
         return True
@@ -105,3 +141,14 @@ def checksol(val):
 main_queue = Queue()
 BFS()
 
+print(prev)
+
+
+while path is not None:
+    print(path)
+    path_array[path[0]][path[1]] = True
+    path = prev[path[0]][path[1]]
+
+
+# path of the solution
+print(path_array)
